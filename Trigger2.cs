edb96d30b55a3770ddd37a28a3410c770d31ad1f
Roman Zayats <95186240+PentaSolEK@users.noreply.github.com>
2024-12-21 03:12:54 +0300

    Update Trigger2.cs

--- Trigger2.cs
+++ Trigger2.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,6 +8,7 @@ public class Trigger2 : MonoBehaviour
         if (other.tag == "Player")
         {
             SceneManager.LoadScene("SampleScene");
+            
         }
     }
 }
